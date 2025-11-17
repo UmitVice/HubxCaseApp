5ece8c80f8c9dc13ea43cdbd0063daad061f905a
Ümit Viçe <umitvice@gmail.com>
2025-11-17 23:37:28 +0300

    Add index file to export all primitive components, streamlining imports for Typography, Button, IconButton, Card, ImageAuto, Spacer, and Divider.

---
+++ src/components/primitives/index.ts
@@ -0,0 +1,9 @@
+export * from './Typography';
+export * from './Button';
+export * from './IconButton';
+export * from './Card';
+export * from './ImageAuto';
+export * from './Spacer';
+export * from './Divider';
+
+
