d63b1bdb7fab41813d5a146eb544705e60bc0d9b
Ümit Viçe <umitvice@gmail.com>
2025-11-17 23:37:05 +0300

    Add navigation type definitions for onboarding and home stacks, enhancing type safety and structure in navigation management.

---
+++ src/navigation/types.ts
@@ -0,0 +1,12 @@
+export type OnboardingStackParamList = {
+	GetStarted: undefined;
+	Onboarding1: undefined;
+	Onboarding2: undefined;
+	Paywall: undefined;
+};
+
+export type HomeStackParamList = {
+	Home: undefined;
+};
+
+
