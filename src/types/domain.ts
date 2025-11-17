7ceebc110d081c104ed9258ca504a2446792e266
Ümit Viçe <umitvice@gmail.com>
2025-11-17 23:41:52 +0300

    Add domain type definitions for Category and QuestionArticle, improving type safety and structure for related data models.

---
+++ src/types/domain.ts
@@ -0,0 +1,17 @@
+export interface Category {
+	id: number;
+	title: string;
+	imageUrl: string;
+	rank: number;
+}
+
+export interface QuestionArticle {
+	id: number;
+	title: string;
+	subtitle: string;
+	imageUri: string;
+	uri: string;
+	order: number;
+}
+
+
